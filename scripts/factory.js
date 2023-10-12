e2907d4a8db9cd9e4117060c676f17f1ed424a07
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-10-12 15:14:51 +0300

    Factory pattern

---
+++ scripts/factory.js
@@ -0,0 +1,42 @@
+// Abstract class PhoneFactory defines the method createPhone(), which should return an object of the Phone class
+class PhoneFactory {
+    createPhone(model) {
+        throw new Error("This method must be overwritten!"); // This throw must be here because we needd to define the phone in the concrete class
+    }
+}
+
+// The concrete class AppleFactory and GoogleFactory override the method createPhone() and return 
+//different types of our products
+class AppleFactory extends PhoneFactory {
+    createPhone(model) {
+        return new Phone(model, 1000);
+    }
+}
+
+class GoogleFactory extends PhoneFactory {
+    createPhone(model) {
+        return new Phone(model, 800);
+    }
+}
+
+// The Phone class represents the product to be created.
+class Phone {
+    constructor(model, price) {
+        this.model = model;
+        this.price = price;
+    }
+}
+
+// Create factories for each type of phone.
+const appleFactory = new AppleFactory();
+const googleFactory = new GoogleFactory();
+
+// Create phones using the factories.
+const phones = [];
+phones.push(appleFactory.createPhone('Apple iPhone'));
+phones.push(googleFactory.createPhone('Google Pixel'));
+
+// Testing
+for (let phone of phones) {
+    console.log(phone.model + ' costs ' + phone.price);
+}
